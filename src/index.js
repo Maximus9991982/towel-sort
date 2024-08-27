aea715076dc30bd71f1869cd896db7cf3318989a
Alexander <150794381+Maximus9991982@users.noreply.github.com>
2024-08-27 22:31:43 +0300

    feat: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-
 module.exports = function towelSort (matrix) {
-    let result = [];
-    
-    for (let i = 0; i <= matrix.length - 1; i++) {
-        if (i == 0 || i%2 == 0) {
-            result.push(matrix[i]);
-            
-        } else {
-            result.push(matrix[i].reverse());
-        }
-    }
-    
-  return result.flat();
+  let result = [];
+  
+//   console.log(arguments.length);
+  
+  if (arguments.length == 0) {
+      result = [];
+  } else {
+  for (let i = 0; i <= matrix.length - 1; i++) {
+      if (i == 0 || i%2 == 0) {
+          result.push(matrix[i]);
+          
+      } else {
+          result.push(matrix[i].reverse());
+      }
+  }
+  result = result.flat();
+  }  
+return result;
 }
